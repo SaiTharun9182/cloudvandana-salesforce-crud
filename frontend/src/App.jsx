b4072faa1173d7f1c82a4de85f550ea1bd7bcb52
C Sai Tharun <saitharunchandypet@gmail.com>
2026-08-19 00:34:50 +0530

    Configure frontend API URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useCallback } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const OBJECTS = [
   "Account",
